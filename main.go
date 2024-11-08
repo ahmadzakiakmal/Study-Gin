45e8927dc258216366293ad632d1439e5b6f2cd8
Ahmad Zaki Akmal <87590846+ahmadzakiakmal@users.noreply.github.com>
2024-11-08 19:25:01 +0700

    feat: create initializers

--- main.go
+++ main.go
@@ -1,13 +1,20 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/ahmadzakiakmal/Study-Gin/initializers"
+	"github.com/gin-gonic/gin"
+)
+
+func init() {
+	initializers.LoadEnv()
+}
 
 func main() {
 	r := gin.Default()
 	r.GET("/ping", func(c *gin.Context) {
 		c.JSON(200, gin.H{
-			"message": "pong",
+			"message": "Hello World!",
 		})
 	})
-	r.Run() // listen and serve on 0.0.0.0:8080
+	r.Run()
 }
